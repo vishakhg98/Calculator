1463d9f2a19ced62dd01a2e3a167c776b8069378
G. Vishakh <60139520+vishakhg98@users.noreply.github.com>
2020-12-03 20:11:00 +0530

    Added History function and fixed many bugs
    Added History of the current calculations.
    Fixed many bugs resulted from this feature.
    Bug fixes:
    1)fixed bug causing same or different operators to be repeated twice, now it will overwrite existing last operator.
    2)Spamming equals gives junk values.
    3) equal wont calculate unless equation is correct. Eg: Calculate(1+2=), Wont calculate (1+=).

--- script1.js
+++ script1.js
@@ -2,60 +2,37 @@
 var prevOutput= document.querySelector('[data-previous-operand]')
 // bottom-Main output box
 var currentOutput= document.querySelector('[data-current-operand]')
-
-// state variable is used to check whether the calculations are fresh or not!
+// console.log(state)
 var state
 function insert(num) {
-  // To clear old calculation history.
   if (state == undefined) {
     state = "using"
-    // console.log("new state")
+    console.log("new state")
     prevOutput.innerText = ''
   }
-
-  // Converting to float at first insert to fix decimal issues.
+  // if first entry replacing it
   if (currentOutput.innerText.indexOf('0') === 0) {
     currentOutput.innerText = parseFloat(currentOutput.innerText)
   }
-  
   // data-numbers
-  if ([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '.'].includes(num)) {
-    if (!currentOutput.innerText.includes(".") || num !== "."){
-      currentOutput.innerText += num
-    }
+  if ([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '.'].indexOf(num) >= 0) {
+    currentOutput.innerText += num
   }
   else {
   // data-operators
   // at input of operator in Bottom-Main output box (currentOutput) moves to Top Output box (prevOutput)
-
-  // if an operator already exists it new-one overwrites the old operator
-    var lastChar = prevOutput.innerText.charAt(prevOutput.innerText.length -1);
-    if (["+", "-", "×", "/"].includes(lastChar) && currentOutput.innerText == ''){
-      // overwriting condition
-      prevOutput.innerText = prevOutput.innerText.replace(/.$/, num);
-    }
-    else {
-      // No need to overwrite condition
-      prevOutput.innerText +=  currentOutput.innerText + num
-      currentOutput.innerText = ""
-    }
+    prevOutput.innerText +=  currentOutput.innerText + num
+    currentOutput.innerText = ""
   }
 }
 
 function equal() {
-  // If only prevOutput is entered and not currentOutput; eg: 1+=
   if (state !== undefined){
-    console.log(1)
     var exp = prevOutput.innerText + currentOutput.innerText
     // console.log(exp)
     if(currentOutput.innerText){
-      // If only prevOutput is entered currentOutput also entered; eg: 1+2 =
-      console.log(2)
-      // adding '=' string after prevOutput history
-        prevOutput.innerText += currentOutput.innerText + " ="
-        // calculating
+        prevOutput.innerText += currentOutput.innerText  
         currentOutput.innerText = eval(exp.replace('×', '*')) // replacing × to * for multiplication.
-        // setting as undefined to clear old calculation history on next button insert.
         state = undefined
     }  
   }
