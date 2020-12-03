d6d95a6bbf891f6abd00e53ab1ca55bc2120a13f
G. Vishakh <60139520+vishakhg98@users.noreply.github.com>
2020-12-03 14:57:40 +0530

    Added Javascript functions and screenshot

---
+++ script.js
@@ -0,0 +1,52 @@
+// Top side output box
+var prevOutput= document.querySelector('[data-previous-operand]')
+// bottom-Main output box
+var currentOutput= document.querySelector('[data-current-operand]')
+
+function insert(num) {
+  // if first entry replacing it
+  if (currentOutput.innerText.indexOf('0') === 0) {
+    currentOutput.innerText = parseFloat(currentOutput.innerText)
+  }
+  // data-numbers
+  if ([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '.'].indexOf(num) >= 0) {
+    currentOutput.innerText += num
+  }
+  else {
+  // data-operators
+  // at input of operator in Bottom-Main output box (currentOutput) moves to Top Output box (prevOutput)
+
+    prevOutput.innerText = prevOutput.innerText + currentOutput.innerText + num
+    currentOutput.innerText = ""
+  }
+}
+
+function equal() {
+  var exp = prevOutput.innerText + currentOutput.innerText
+  // console.log(exp)
+  if(currentOutput.innerText){
+      currentOutput.innerText = eval(exp.replace('×', '*')) // replacing × to * for multiplication.
+      prevOutput.innerText = ""
+  }
+}
+
+function clearall() {
+  // Restitng to 0
+  prevOutput.innerText = ""
+  currentOutput.innerText = 0
+}
+
+function backspace() {
+  // Deleting last character of Bottom-Main output
+  if(currentOutput.innerText){
+    currentOutput.innerText = currentOutput.innerText.slice(0, -1);
+  }
+  // Deleting last character of Top output
+  else if(prevOutput.innerText) {
+    prevOutput.innerText = prevOutput.innerText.slice(0, -1);
+  }
+  // When Bottom-Main output empty will reset to 0
+  if (currentOutput.innerText === "" && prevOutput.innerText === ""){
+    currentOutput.innerText = 0
+  }
+}
